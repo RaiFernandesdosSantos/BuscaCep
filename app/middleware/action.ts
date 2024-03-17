15f397287acb03d9545aac15a2d8060d5278317d
RaiFernandesdosSantos <raifernandes076@gmail.com>
2024-03-17 16:53:31 -0400

    Middleware terminado

--- app/middleware/action.ts
+++ app/middleware/action.ts
@@ -1,12 +1,15 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export async function buscar(fData: FormData) {
+export async function Buscar(fData: FormData) {
   const busca = fData.get("rc");
 
   const response = await fetch(`https://viacep.com.br/ws/${busca}/json/`);
   const data = await response.json();
 
   cookies().set("cep", JSON.stringify(data));
+
+  redirect("/resultado");
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Header from "@/public/components/header";
 import "@/public/style/style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
-import { buscar } from "./middleware/action";
+import { Buscar } from "./middleware/action";
 
 export default function Home() {
   return (
     <div className="container">
       <Header />
       <main className="main">
-        <form action={buscar}>
+        <form action={Buscar}>
           <h1>Buscar CEP</h1>
           <label htmlFor="rc">Digite o CEP que deseja pesquisar:</label>
           <input
